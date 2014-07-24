3e2bc26a03ed28b8d100bfc34fc65abe54d66f9c
tomgreenfield <tomgreenfield@users.noreply.github.com>
2014-07-24 10:48:11 +0100

    Refactored run_command_here

--- AdaptHelper.py
+++ AdaptHelper.py
@@ -1,31 +1,40 @@
 import sublime
 import sublime_plugin
 
+
 class RunCommandHereCommand(sublime_plugin.WindowCommand):
 	def run(self, **args):
 		self.dir = args["dirs"][0]
-		if args.get("require_input"):
-			if args.get("cmd_name"):
-				self.cmd_name = args["cmd_name"]
-				self.window.show_input_panel("$ " + self.cmd_name, "", self.make_cmd, None, None)
-			else:
-				self.window.show_input_panel("$", "", self.on_done, None, None)
-		elif args.get("cmd_name"):
-			self.on_done(args["cmd_name"])
+		self.cmd_name = ""
+		self.separator = ""
+		label = None
 
-	def make_cmd(self, user_args):
-		if self.cmd_name == "grunt": self.cmd_name += " --no-color"
-		if user_args: self.cmd_name += " " + user_args
-		self.on_done(self.cmd_name)
+		if args.get("cmd_name"): self.cmd_name = args["cmd_name"]
+		if args.get("label"): label = args["label"]
+		if args.get("separator"): self.separator = args["separator"]
 
-	def on_done(self, cmd_name):
+		if self.cmd_name and label:
+			self.window.show_input_panel(label, "", self.check_input, None, None)
+		elif label:
+			self.window.show_input_panel(label, "", self.execute, None, None)
+		else:
+			self.execute(self.cmd_name)
+
+	def check_input(self, input):
+		if input: self.cmd_name += self.separator + input
+		self.execute(self.cmd_name)
+
+	def execute(self, cmd_name):
 		if cmd_name:
-			self.window.run_command("exec", {"cmd": cmd_name, "shell": True, "working_dir": self.dir})
+			self.window.run_command("exec", {
+				"cmd": cmd_name, "shell": True, "working_dir": self.dir
+			})
 			sublime.status_message("Running " + cmd_name)
 
 	def is_visible(self, **args):
 		return len(args["dirs"]) > 0
 
+
 class KillCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command("exec", {"kill": True})
+	def run(self):
+		self.window.run_command("exec", {"kill": True})
