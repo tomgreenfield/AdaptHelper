55c0da7362d86ab93fe28d2f7e85b5ee404cd570
tomgreenfield <tomgreenfield@users.noreply.github.com>
2015-05-05 11:24:42 +0100

    Added redo command

--- AdaptHelper.py
+++ AdaptHelper.py
@@ -4,6 +4,8 @@ import os.path
 
 class RunCommandHereCommand(sublime_plugin.WindowCommand):
 	def run(self, **args):
+		if args.get("redo"): return self.execute(None)
+
 		self.dir = args["dirs"][0]
 		self.cmd_name = args.get("cmd_name", "")
 		self.separator = args.get("separator", "")
@@ -25,6 +27,8 @@ class RunCommandHereCommand(sublime_plugin.WindowCommand):
 		sublime.status_message("Running " + self.cmd_name)
 
 	def is_visible(self, **args):
+		if args.get("redo"): return hasattr(self, "cmd_name")
+
 		dirs = args["dirs"]
 		cmd_name = args.get("cmd_name")
 
@@ -36,3 +40,8 @@ class RunCommandHereCommand(sublime_plugin.WindowCommand):
 		else: return True
 
 		return os.path.isfile(os.path.join(dirs[0], required_file))
+
+	def description(self, **args):
+		if args.get("redo") and hasattr(self, "cmd_name"):
+			return "Redo \"" + self.cmd_name + "\""
+		return ""
